5cd4c0dec28e224d5d9f14e8a25ad18528a38a30
Sahil Shende <ssahilshende@gmail.com>
2021-07-02 22:16:15 +0530

    complete challenge 17

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* REQUIREMENTS IN PROJECTS */
 const express = require("express");
 const ejs = require("ejs");
 const PORT = 3000;
@@ -11,15 +12,25 @@ app.use(express.urlencoded({extended: true}));
 //Using static files
 app.use(express.static("public"));
 
+/* --- GLOBAL DECLARATIONS --- */
+
+/* variables */
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.const homeStartingContent Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";;
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+/* arrays */
+
+const posts = [];
+
+/* --- ROUTES --- */
 
 /* HOME ROUTE */
 app.get("/", (req, res)=>{
 
   res.render("home",{
-    home_start_content : homeStartingContent
+    home_start_content : homeStartingContent,
+    posts : posts
   });
 });
 
@@ -49,8 +60,24 @@ app.post("/compose",(req, res)=>{
     title : req.body.postTitle,
     content : req.body.postBody
   };
-  res.redirect("/compose");
-})
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:title",(req, res)=>{
+  const requestTitle = req.params.title;
+  let i;
+  for(i=0; i<posts.length; i++){
+    if(posts[i].title.toLowerCase === requestTitle.toLowerCase){
+      break;
+    }
+  }
+  if(i==posts.length)
+    console.log("NOT MATCHED");
+  else
+    console.log("MATCHED");
+
+});
 
 
 
